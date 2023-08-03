c8afb485939a54a029e5499a6cc58040e5ecd7ad
e2203080 <e2203080@edu.vamk.fi>
2023-08-03 19:26:32 +0300

    change

---
+++ game_of_life_doxy.c
@@ -0,0 +1,27 @@
+/**
+ * @file game_of_life.c
+ * @brief Game of Life - a simple version.
+ */
+
+/**
+ * @mainpage Game of Life - Simple Version
+ *
+ * @section intro_sec Introduction
+ * This program is a simple implementation of Conway's Game of Life, a cellular automaton simulation.
+ * It evolves according to certain rules based on the initial configuration of cells.
+ *
+ * @section usage_sec Usage
+ * - Compile: gcc -o game_of_life game_of_life.c
+ * - Run: ./game_of_life
+ */
+
+/**
+ * @file game_of_life.c
+ * @brief Game of Life - a simple version.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 
